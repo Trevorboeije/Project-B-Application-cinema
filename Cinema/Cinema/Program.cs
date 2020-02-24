7f3874bfd0a67ec5b58ca031571db6cfc1252b57
Mattias <m.heijboer2@students.uu.nl>
2020-02-24 01:12:44 +0100

    lezen van zalen uit .txt-bestand
    het programma kan nu de layout en het aantal stoelen in de zaal uit een .txt lezen

---
+++ Cinema/Cinema/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zaal zaalone = new Zaal(System.IO.File.ReadAllLines(@".\zalen\zaal1.txt"));
+            Zaal zaaltwo = new Zaal(System.IO.File.ReadAllLines(@".\zalen\zaal2.txt"));
+            Zaal zaalthree = new Zaal(System.IO.File.ReadAllLines(@".\zalen\zaal3.txt"));
+        }
+    }
+
+    class Zaal
+    {
+        char[,] layout;
+        int chairs;
+        public Zaal(string[] l)
+        {
+            chairs = Convert.ToInt32(l[l.Length - 1]);
+            char[,] inputMatrix = new char[l[0].Length, l.Length - 1];
+            for (int i = 0; i < inputMatrix.GetLength(0); i++)
+                for (int j = 0; j < inputMatrix.GetLength(1); j++)
+                {
+                    inputMatrix[i, j] = l[j][i];
+                }
+            layout = inputMatrix;
+        }
+    }
+}
