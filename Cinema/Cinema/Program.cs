d5e23cfd1d867d1bcd38dc29ef91adec195b5107
Mattias <m.heijboer2@students.uu.nl>
2020-03-17 19:28:48 +0100

    JSON conversie
    De zalen worden nu gelezen vanuit een JSON bestand

--- Cinema/Cinema/Program.cs
+++ Cinema/Cinema/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 namespace Cinema
 {
@@ -6,24 +8,26 @@ namespace Cinema
     {
         static void Main(string[] args)
         {
-            Zaal zaalone = new Zaal(System.IO.File.ReadAllLines(@".\zalen\zaal1.txt"));
-            Zaal zaaltwo = new Zaal(System.IO.File.ReadAllLines(@".\zalen\zaal2.txt"));
-            Zaal zaalthree = new Zaal(System.IO.File.ReadAllLines(@".\zalen\zaal3.txt"));
+            Zaal zaalone = new Zaal(System.IO.File.ReadAllText(@".\rooms\zaal1.json"));
+            Zaal zaaltwo = new Zaal(System.IO.File.ReadAllText(@".\rooms\zaal2.json"));
+            Zaal zaalthree = new Zaal(System.IO.File.ReadAllText(@".\rooms\zaal3.json"));
         }
     }
 
     class Zaal
     {
-        char[,] layout;
-        int chairs;
-        public Zaal(string[] l)
+        readonly char[,] layout;
+        readonly int chairs;
+        public Zaal(string l)
         {
-            chairs = Convert.ToInt32(l[l.Length - 1]);
-            char[,] inputMatrix = new char[l[0].Length, l.Length - 1];
+            JObject input = JObject.Parse(l);
+            chairs = (int)input["chairs"];
+            JArray inputJArray = JArray.Parse(input["layout"].ToString());
+            char[,] inputMatrix = new char[inputJArray.First.ToString().Length, inputJArray.Count];
             for (int i = 0; i < inputMatrix.GetLength(0); i++)
                 for (int j = 0; j < inputMatrix.GetLength(1); j++)
                 {
-                    inputMatrix[i, j] = l[j][i];
+                    inputMatrix[i, j] = inputJArray[j].ToString()[i];
                 }
             layout = inputMatrix;
         }
