efbfcbd1412f50040d184571925859677e37460c
Mattias <m.heijboer2@students.uu.nl>
2020-03-26 17:40:33 +0100

    Classes toegevoegd
    Room is nu z'n eigen document, scheduleElement is functioneel

---
+++ Cinema/Cinema/Room.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace Cinema
+{
+    class Room
+    {
+        char[,] layout;
+        int chairs;
+        public Room(string l)
+        {
+            //the actual initialization function is its own method so that it can be called manually
+            Initialize(l);
+        }
+        public void deleteRoom()
+        {
+            //TODO: D31373 the room
+        }
+        public void Initialize(string l)
+        {
+            //this line takes the string and turns it into a special object that contains the attributes of the JSON
+            JObject input = JObject.Parse(l);
+            //this line assigns the chairs value stored in the file in the object's chair variable 
+            chairs = (int)input["chairs"];
+            //there's also an array of strings in the file: this file takes it and turns it from a massive string to a special array
+            JArray inputJArray = JArray.Parse(input["layout"].ToString());
+            //create an empty 2D character array the same size as the string array
+            char[,] inputMatrix = new char[inputJArray.First.ToString().Length, inputJArray.Count];
+            // go through each position in the character array and fill it with the proper character
+            for (int i = 0; i < inputMatrix.GetLength(0); i++)
+                for (int j = 0; j < inputMatrix.GetLength(1); j++)
+                {
+                    inputMatrix[i, j] = inputJArray[j].ToString()[i];
+                }
+            //store the array in the object
+            layout = inputMatrix;
+        }
+
+        public void updateCreateRoom(string room)
+        {
+            //read the file as one big string and turn it into a special object
+            JObject fullObject = JObject.Parse(File.ReadAllText(room));
+            //read the array in the object and store it
+            JArray layoutArray = (JArray)fullObject["layout"];
+            //go through each row in the layout
+            for (int i = 0; i < layoutArray.Count; i++)
+            {
+                Console.WriteLine("Replace the " + (i + 1) + " line? If yes give new line.");
+                string newLine = Console.ReadLine();
+                if (newLine != "")
+                {
+                    //TODO: does making changes to layoutarray change the array inside fullObject?
+                    layoutArray[i] = newLine;
+                }
+            }
+
+            Console.WriteLine("How many chairs should the room have?");
+            //alter the special object
+            fullObject["chairs"] = Int32.Parse(Console.ReadLine());
+            //turn the object into a string
+            string updatedString = fullObject.ToString();
+            //update the object
+            Initialize(updatedString);
+            //update the file
+            File.WriteAllText(room, updatedString);
+        }
+    }
+}
---
+++ Cinema/Cinema/ScheduleElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cinema
+{
+    class ScheduleElement
+    {
+        string time;
+        string movie;
+        Room room;
+        string date;
+
+        public ScheduleElement(string t, string m, Room r, string d)
+        {
+            time = t;
+            movie = m;
+            room = r;
+            date = d;
+        }
+
+        public void printScheduleElement()
+        {
+            Console.WriteLine(string.Format("Playing {0} at {1} on {2} in Maasvlakte {3}", movie, time, date, Program.rooms.IndexOf(room)));
+        }
+    }
+}
